cdbcff81df6e1d511cd689f5b78cbe541f5f446a
saropi99 <saropi9978@gmail.com>
2021-08-28 18:14:14 -0500

    project created

---
+++ Game/src/main/java/com/javeriana/Game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
