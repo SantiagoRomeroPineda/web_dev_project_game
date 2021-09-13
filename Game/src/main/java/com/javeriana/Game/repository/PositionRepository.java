3ac3e69d1b49178662ad6527b0d7548e8345287d
saropi99 <saropi9978@gmail.com>
2021-09-12 23:55:24 -0500

    DB changes

--- Game/src/main/java/com/javeriana/Game/repository/PositionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javeriana.Game.repository;
-import com.javeriana.Game.model.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PositionRepository  extends JpaRepository<Position, Long> {
-}
